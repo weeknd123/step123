543b5cf6cd3eaf7e4c1d73365d0e84ccc9f09577
weeknd123 <bohdan.vasin@gmail.com>
2022-11-19 11:45:34 -0500

    hi

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ class Student:
         self.name = name
     def __str__(self):
         return f"I am a student. My name is {self.name}."
+    def __del__(self):
+        print("Training is over. I am now an expert!")
 
 nick = Student(name="Nick")
 print(nick)
