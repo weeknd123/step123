ef43f3466cf8b798bbd476c5da3e8d09bc762b10
weeknd123 <bohdan.vasin@gmail.com>
2022-11-19 12:12:49 -0500

    class

--- main.py
+++ main.py
@@ -1,14 +1,53 @@
+import random
 class Student:
-    def __init__(self, name=None, height=160):
+    def __init__(self, name):
         self.name = name
-        self.height = height
-    def __bool__(self):
-        return self.name != None
-    def __len__(self):
-        return self.height
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+    def to_study(self):
+        print("Time to study")
+        self.progress += 0.12
+        self.gladness -= 3
+    def to_sleep(self):
+        print("I will sleep")
+        self.gladness +=5
+    def to_chill(self):
+        print("Rest time")
+        self.gladness += 5
+        self.progress -= 0.1
 
-nick = Student()
-print(nick.__len__())
-print(nick.__bool__())
-print(len(nick))
-print(bool(nick))
+    def is_alive(self):
+        if self.progress < -0.5:
+            print("Cast Out...")
+            self.alive = False
+        elif self.gladness <=0:
+            print("Depression...")
+            self.alive = False
+        elif self.progress > 5:
+            print("Passed externally...")
+            self.alive = False
+
+    def end_of_day(self):
+        print(f"Gladness = {self.gladness}")
+        print(f"Progress = {round(self.progress, 2)}")
+
+    def live(self, day):
+        day = "Day " + str(day) + " of " + self.name + " life "
+        print(f"{day:=^50}")
+        live_cube = random.randint(1,4)
+        if live_cube == 1 or live_cube == 2 :
+            self.to_study()
+        elif live_cube == 3:
+            self.to_sleep()
+        elif live_cube == 4:
+            self.to_chill()
+        self.end_of_day()
+        self.is_alive()
+
+nick = Student(name="Cristiano")
+for day in range(365):
+    if nick.alive ==  False:
+        break
+    nick.live(day)
+
