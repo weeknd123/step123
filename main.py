57225e813ee434586f5884862f6d211a086f9e93
weeknd123 <bohdan.vasin@gmail.com>
2022-11-19 11:43:58 -0500

    hi

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hi")
+class Student:
+    def __init__(self, name=None):
+        self.name = name
+    def __str__(self):
+        return f"I am a student. My name is {self.name}."
+
+nick = Student(name="Nick")
+print(nick)
